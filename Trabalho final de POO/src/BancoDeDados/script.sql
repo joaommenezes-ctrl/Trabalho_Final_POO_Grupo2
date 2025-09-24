5b68bd05f7ef900db4effde35fb3ac6ea96d75a3
João Gabriel Menezes Marra <joaommenezes100@gmail.com>
2025-09-23 21:23:39 -0300

    Update script.sql

--- Trabalho final de POO/src/BancoDeDados/script.sql
+++ Trabalho final de POO/src/BancoDeDados/script.sql
@@ -15,15 +15,6 @@ CREATE TYPE ProjetoFinal.Parentesco_Enum AS ENUM (
     'FILHO', 'SOBRINHO', 'OUTROS'
 );
 
-CREATE TABLE ProjetoFinal.Dependente (
-    id SERIAL PRIMARY KEY,
-    nome TEXT NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    data_nascimento DATE NOT NULL,
-    parentesco ProjetoFinal.Parentesco_Enum NOT NULL,
-    funcionario_id INTEGER NOT NULL REFERENCES ProjetoFinal.Funcionario(id)
-);
-
 CREATE TABLE ProjetoFinal.FolhaPagamento (
     id SERIAL PRIMARY KEY,
     funcionario INTEGER NOT NULL REFERENCES ProjetoFinal.Funcionario(id),
@@ -33,12 +24,6 @@ CREATE TABLE ProjetoFinal.FolhaPagamento (
     salario_liquido NUMERIC(15,2) NOT NULL
 );
 
-ALTER TABLE ProjetoFinal.Funcionario
-ADD COLUMN desconto_inss NUMERIC(15,2) NOT NULL DEFAULT 0,
-ADD COLUMN desconto_ir NUMERIC(15,2) NOT NULL DEFAULT 0;
-
-ALTER TABLE ProjetoFinal.Funcionario
-ADD COLUMN salario_liquido NUMERIC(15,2) NOT NULL DEFAULT 0;
 
 CREATE TABLE IF NOT EXISTS ProjetoFinal.Dependentes(
     id SERIAL PRIMARY KEY,
@@ -48,7 +33,6 @@ CREATE TABLE IF NOT EXISTS ProjetoFinal.Dependentes(
     data_nascimento DATE NOT NULL,
     cod_funcionario INTEGER REFERENCES ProjetoFinal.Funcionario(id)
 );
-
 ALTER TABLE ProjetoFinal.Funcionario
     ADD COLUMN salario_bruto NUMERIC(15,2) NOT NULL DEFAULT 0;
 
@@ -59,6 +43,7 @@ SELECT * FROM ProjetoFinal.Dependentes;
 SELECT f.id, f.nome, f.cpf, f.data_nascimento, fp.data_pagamento, fp.desconto_inss, fp.desconto_ir, fp.salario_liquido
 FROM ProjetoFinal.Funcionario f
 LEFT JOIN ProjetoFinal.FolhaPagamento fp on fp.funcionario = f.id;
+
 
 
 
